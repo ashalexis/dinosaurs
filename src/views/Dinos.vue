63392424ca7b259aff1b6857ffbee18425ce15f0
Ashley Lim <ashalexis1297@gmail.com>
2021-01-10 13:23:59 +1100

    dino directory grid

--- src/views/Dinos.vue
+++ src/views/Dinos.vue
@@ -1,23 +1,88 @@
 <template>
-  <div id="dinos">
-    <div id="dinos-header">
-      <h1>Dinos!</h1>
-      <v-spacer />
-      <v-btn to="/create" color="success">Add a dino!</v-btn>
-    </div>
-    <DinoTable :dinosaurs="sortedDinos" />
+  <div id="dino-directory">
+    <v-container>
+      <v-row>
+        <v-col sm="12" md="3">
+          <v-text-field
+            outlined
+            single-line
+            label="Search dinosaurs"
+            append-icon="mdi-database-search"
+            v-model="singlesearch"
+          ></v-text-field>
+
+          <v-menu :close-on-content-click="false">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn text outlined v-bind="attrs" v-on="on">
+                Explore dinosaurs by:
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item>
+                <v-select
+                  v-model="formData.name"
+                  :items="alphabet"
+                  label="Name (A-Z)"
+                  prepend-inner-icon="mdi-plus"
+                >
+                </v-select>
+              </v-list-item>
+
+              <v-list-item>
+                <v-select
+                  v-model="formData.era"
+                  :items="era"
+                  label="When they lived"
+                  prepend-inner-icon="mdi-plus"
+                >
+                </v-select>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-col>
+
+        <v-col sm="12" md="9">
+          <h2>All dinosaurs</h2>
+          <p>Laksjfskfkajshflkashdfkhfaskjdhfaksdhf</p>
+          <p></p
+        ></v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-import DinoTable from "@/components/DinoTable.vue";
+function genCharArray(charA, charZ) {
+  var a = [],
+    i = charA.charCodeAt(0),
+    j = charZ.charCodeAt(0);
+  for (; i <= j; ++i) {
+    a.push(String.fromCharCode(i));
+  }
+  return a;
+}
 
 export default {
-  components: {
-    DinoTable,
-  },
   data() {
-    return {};
+    return {
+      singlesearch: "",
+      formData: {
+        name: "",
+        era: "",
+      },
+      alphabet: [],
+      era: [
+        "Late Triassic",
+        "Early Jurassic",
+        "Mid Jurassic",
+        "Late Jurassic",
+        "Early Cretaceous",
+        "Late Cretaceous",
+      ],
+    };
+  },
+  created() {
+    this.alphabet = genCharArray("A", "Z");
   },
   mounted() {},
   computed: {
@@ -28,12 +93,4 @@ export default {
 };
 </script>
 
-<style>
-#dinos {
-  padding: 1rem;
-}
-
-#dinos-header {
-  display: flex;
-}
-</style>
+<style></style>
